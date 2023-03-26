c3a28519e0d05948d18888fa2fcf932679832aa5
robelabiye <rblabiye5@gmail.com>
2023-03-26 23:56:39 +0300

    added functoin for game play and restructred the code

--- script.js
+++ script.js
@@ -1,5 +1,65 @@
-const result= document.getElementById("result")
+  function game() {
+    /** change it with a function 
+      * the function should check if the player's input are only the 3 choices
+      * and the player's input should convert to lower cases
+    */
+    let playerSelection   = prompt("rock, paper, scissor").toLowerCase()
+    let computerSelection = getComputerChoice();
 
-const userInput = prompt("Rock, Paper, Sciassor")
+    playRound(playerSelection, computerSelection)
+  }
 
-result.textContent= userInput;
+/**Function to return computer choices */
+
+  function getComputerChoice() {
+
+    //setting computer choice to the numbers 1,2 or 3
+    let computerChoice =  Math.floor(Math.random()*3+1)
+
+    return computerChoice === 1 ? "rock"
+            : computerChoice === 2 ? "paper"
+            : "scissor";
+
+  }
+
+
+/**Function to take two choices and return a winner */
+
+function playRound(player, computer) {
+
+  if (player ==="rock") {
+    if (computer === "rock") {
+      return " DRAW 🟰"
+    }
+    else if (computer === "scissor") {
+      return "You Win 🎇!  rock beats scissor"
+    }
+    else if (computer === "paper") {
+      return "You Loose 😭! paper beats rock"
+    }
+  }
+
+  if (player ==="paper") {
+    if (computer === "paper") {
+      return " DRAW 🟰"
+    }
+    else if (computer === "rock") {
+      return "You Win 🎇! paper paper rock "
+    }
+    else if (computer === "scissor") {
+      return "You Win 😭! scissor beats paper"
+    }
+  }
+
+  if (player ==="scissor") {
+    if (computer === "scissor") {
+      return " DRAW 🟰"
+    }
+    else if (computer === "rock") {
+      return "You Win 🎇! rock beats scissor"
+    }
+    else if (computer === "paper") {
+      return "You Win 🎇! scissor beats paper"
+    }
+  }
+}
