1dc6d9b554c3bfd0f62a5d1ffc5ca03b846956d5
robelabiye <rblabiye5@gmail.com>
2023-05-07 12:50:58 +0300

    Added Styles and Changed the game logic to fit for UI

--- script.js
+++ script.js
@@ -1,57 +1,62 @@
-const score_bord = document.querySelector(".score")
-const round_result = document.querySelector(".result")
-const play_buttons = document.querySelectorAll(".play_button")
+const computer_score = document.querySelector(".computer_score");
+const player_score = document.querySelector(".player_score");
+const play_buttons = document.querySelectorAll(".play_button");
+const winner_tab = document.querySelector(".winner_tab")
 
-play_buttons.forEach(button => {
+/** Assigning event listners to the play buttons*/
+play_buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    playRound(button.value)
-  })
+    playRound(button.value);
+  });
 });
 
-
 /**Function to return computer choices */
-
 function getComputerChoice() {
-
   /**setting computer choice to the numbers 1,2 or 3*/
   let computerChoice = Math.floor(Math.random() * 3 + 1);
 
   return computerChoice === 1
-    ? "rock" : computerChoice === 2
-      ? "paper" : "scissor";
+    ? "rock"
+    : computerChoice === 2
+    ? "paper"
+    : "scissor";
 }
 
-let playerScore =0;
-let computerScore=0;
+let playerScore = 0;
+let computerScore = 0;
 
-function playRound(playWith) {
-  let computer = getComputerChoice()
-  currentWinner = rockPaperScissor(playWith, computer)
+function playRound(choice) {
+  let computersChoice = getComputerChoice();
+  currentWinner = rockPaperScissor(choice, computersChoice);
 
-  round_result.textContent = currentWinner;
+  // round_result.textContent = currentWinner;
 
   if (currentWinner === "player") {
     playerScore++;
+    player_score.textContent = playerScore.toString();
   }
 
   if (currentWinner === "computer") {
     computerScore++;
+    computer_score.textContent = computerScore.toString();
   }
 
-  if (computerScore == 5) {
-    score_bord.textContent = "Winner computer"
-    playerScore = 0; computerScore=0;
+  if (computerScore === 5) {
+    winner_tab.textContent = "You Loss";
+    playerScore = 0;
+    computerScore = 0;
   }
-  if (playerScore == 5) {
-    score_bord.textContent = "Winner player"
-    playerScore = 0; computerScore=0;
+  if (playerScore === 5) {
+    winner_tab.textContent = "You Win";
+    playerScore = 0;
+    computerScore = 0;
   }
-  console.log(computerScore, playerScore)
 }
 
 function rockPaperScissor(player, computer) {
-
-  if (player === computer) { return "Tie!"; }
+  if (player === computer) {
+    return "Tie!";
+  }
 
   if (player === "rock") {
     if (computer === "paper") {
